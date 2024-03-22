390355af11b74a8f11d8ea4edd272493c06edeaa
madhvantyagi <90159839+madhvantyagi@users.noreply.github.com>
2024-03-21 23:32:43 -0400

    This updation I connected it with OpenAi Assistant and method to create threads

---
+++ OpenAi/Messageout.js
@@ -0,0 +1,46 @@
+import { OpenAI } from "openai";
+import { config } from "dotenv";
+import fs from "fs";
+config();
+import { threadCreate } from "./OpenAi.js";
+
+const assistant_id = process.env.ASSISTANT_ID;
+const api_id = process.env.API_KEY;
+
+const openai = new OpenAI({
+  apiKey: api_id,
+});
+
+const thread = await threadCreate();
+const threadOutput = async (UserMessage) => {
+  //This line passing message to that specific thread (thread is nothing but just a queue of chat for a user)
+  const messagess = await openai.beta.threads.messages.create(thread, {
+    role: "user",
+    content: UserMessage,
+  });
+
+  // This thread is getting processed by GPT 3.5
+  const run = await openai.beta.threads.runs.create(thread, {
+    assistant_id: assistant_id,
+    model: "gpt-3.5-turbo-1106",
+    tools: [{ type: "retrieval" }],
+  });
+
+  // This function just check the status of our thread , reason we have to run a loop is because it waits and we can't wait asynchronously it has to be synchronous
+  async function waitCompleteion(threadId, runId) {
+    let status = "queued";
+    while (status === "queued" || status === "in_progress") {
+      const run2 = await openai.beta.threads.runs.retrieve(threadId, runId);
+      status = run2.status;
+      console.log(status);
+      if (status === "completed") {
+        return run2;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 seconds delay
+    }
+  }
+  await waitCompleteion(thread, run.id);
+
+  const messages = await openai.beta.threads.messages.list(thread);
+  console.log(messages.body.data[0].content);
+};
---
+++ OpenAi/OpenAi.js
@@ -0,0 +1,24 @@
+import { OpenAI } from "openai";
+import { config } from "dotenv";
+import fs from "fs";
+config();
+import readline from "readline";
+
+const assistant_id = process.env.ASSISTANT_ID;
+const api_id = process.env.API_KEY;
+
+const openai = new OpenAI({
+  apiKey: api_id,
+});
+
+const threadCreate = async () => {
+  const assistant = await openai.beta.assistants.retrieve(assistant_id);
+  const thread = await openai.beta.threads.create();
+  const data = thread.id + "%" + assistant.id;
+  console.log(thread);
+  return thread.id;
+};
+
+export { threadCreate };
+
+//what professor said about transaction and concurrency
